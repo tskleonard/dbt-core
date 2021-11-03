9d9cfaff4bf96821a6dc0c66bad82dea4ef2b6f7
Jeremy Cohen <jeremy@dbtlabs.com>
2021-11-03 12:40:15 +0100

    Last few test fixes

--- core/dbt/include/global_project/macros/materializations/models/table/create_table_as.sql
+++ core/dbt/include/global_project/macros/materializations/models/table/create_table_as.sql
@@ -3,24 +3,23 @@
 {%- endmacro %}
 
 {% macro default__get_create_table_as_sql(temporary, relation, sql) -%}
-  {%- set sql_header = config.get('sql_header', none) -%}
-
-  {{ sql_header if sql_header is not none }}
-
-  create {% if temporary: -%}temporary{%- endif %} table
-    {{ relation.include(database=(not temporary), schema=(not temporary)) }}
-  as (
-    {{ sql }}
-  );
-
+  {{ return(create_table_as(temporary, relation, sql)) }}
 {% endmacro %}
 
 
-/* {# old name for backwards compatibility #} */
+/* {# keep logic under old macro name for backwards compatibility #} */
 {% macro create_table_as(temporary, relation, sql) -%}
   {{ adapter.dispatch('create_table_as', 'dbt')(temporary, relation, sql) }}
 {%- endmacro %}
 
 {% macro default__create_table_as(temporary, relation, sql) -%}
-  {{ return(get_create_table_as_sql(temporary, relation, sql)) }}
+  {%- set sql_header = config.get('sql_header', none) -%}
+  
+  {{ sql_header if sql_header is not none }}
+  
+  create {% if temporary: -%}temporary{%- endif %} table
+    {{ relation.include(database=(not temporary), schema=(not temporary)) }}
+  as (
+    {{ sql }}
+  );
 {%- endmacro %}
--- core/dbt/include/global_project/macros/materializations/models/view/create_view_as.sql
+++ core/dbt/include/global_project/macros/materializations/models/view/create_view_as.sql
@@ -3,20 +3,20 @@
 {%- endmacro %}
 
 {% macro default__get_create_view_as_sql(relation, sql) -%}
+  {{ return(create_view_as(relation, sql)) }}
+{% endmacro %}
+
+
+/* {# keep logic under old name for backwards compatibility #} */
+{% macro create_view_as(relation, sql) -%}
+  {{ adapter.dispatch('create_view_as', 'dbt')(relation, sql) }}
+{%- endmacro %}
+
+{% macro default__create_view_as(relation, sql) -%}
   {%- set sql_header = config.get('sql_header', none) -%}
 
   {{ sql_header if sql_header is not none }}
   create view {{ relation }} as (
     {{ sql }}
   );
-{% endmacro %}
-
-
-/* {# old name for backwards compatibility #} */
-{% macro create_view_as(relation, sql) -%}
-  {{ adapter.dispatch('create_view_as', 'dbt')(relation, sql) }}
-{%- endmacro %}
-
-{% macro default__create_view_as(relation, sql) -%}
-  {{ return(get_create_view_as_sql(relation, sql)) }}
 {%- endmacro %}
