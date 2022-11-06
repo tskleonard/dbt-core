492f0bf8dd9ac4d1498a3c047b4c605015f914c2
timle <aplenty_blocs_0z@icloud.com>
2022-11-06 17:56:14 -0500

    simplify/mirror hub install pattern
    simplify with goal of mirroring hub install pattern
    - supporting subfolders like git packages, and sha1 checks are removed
    - existing code from RegistryPinnedPackage (install() and download_and_untar()) performs the operations
    - RegistryPinnedPackage install() and download_and_untar() are not currently set up as functions that can be used across classes - this should be moved to dbt.deps.base, or to a dbt.deps.common file - need dbt labs feedback on how to proceed (or leave as is)

--- core/dbt/deps/tarball.py
+++ core/dbt/deps/tarball.py
@@ -1,33 +1,11 @@
-import os
-
 import functools
-import tarfile
+import os
+from typing import Optional
 
-# from tempfile import NamedTemporaryFile
-from typing import Optional  # , Union, BinaryIO
 from dbt.clients import system
+from dbt.contracts.project import RegistryPackageMetadata, TarballPackage
 from dbt.deps.base import PinnedPackage, UnpinnedPackage, get_downloads_path
-from dbt.exceptions import (
-    DependencyException,
-    package_structure_malformed,
-    package_sha1_fail,
-)
-from dbt.events.functions import fire_event
-from dbt.events.types import (
-    Sha1ChecksumPasses,
-    UntarProjectRoot,
-    TarballReceivedFeedback,
-    CopyFeedback,
-)
-from dbt.config import Project
-from dbt.contracts.project import (
-    ProjectPackageMetadata,
-    TarballPackage,
-)
 from dbt.utils import _connection_exception_retry as connection_exception_retry
-
-
-TARFILE_MAX_SIZE = 1 * 1e6  # limit tarfiles to 1mb
 
 
 class TarballPackageMixin:
@@ -47,212 +25,114 @@ class TarballPinnedPackage(TarballPackageMixin, PinnedPackage):
     def __init__(
         self,
         tarball: str,
-        sha1: Optional[str] = None,
-        subdirectory: Optional[str] = None,
+        package: str,
+        version: Optional[str] = None,
     ) -> None:
         super().__init__(tarball)
-        self.sha1 = sha1
-        self.subdirectory = subdirectory
-        self.prep_tarfile()
+        # setup to recycle RegistryPinnedPackage fns
+        self.package = package
+        self.version = version
+
+    @property
+    def name(self):
+        return self.package
 
     def get_version(self):
-        return None
+        return self.version
 
     def nice_version_name(self):
-        return "<tarball @ {}>".format(self.tarball)
-
-    def get_temp_name(self):
-        import tempfile
-
-        return next(tempfile._get_candidate_names())
-
-    def prep_tarfile(self):
-        def validate_checksum(self, filepath: str):
-            if self.sha1:
-                checksum = system.file_sha1(filepath)
-                if not checksum == self.sha1:
-                    package_sha1_fail(filepath, checksum, self.sha1)
-                fire_event(Sha1ChecksumPasses(sha1=self.sha1, filepath=filepath))
-
-        def validate_tarfile(self, filepath: str):
-            if not tarfile.is_tarfile(filepath):
-                msg = f"{filepath} is not a valid tarfile."
-                raise DependencyException(msg)
-
-        # def handle_local_tar(self):
-        #     # basic tarfile path passthrough primarily for testing
-        #     # validate_checksum(self, self.tarball)
-        #     # validate_tarfile(self, self.tarball)
-        #     self.tarfile_path = self.tarball
-
-        # def handle_remote_tar(self):
-        #     """copied from /dbt/deps/registry.py install
-        #     However we can't just package download/untar/install together as one
-        #     step like in registry. With registry we can simply untar into the
-        #     packages folder. Here, first we need to download and untar to temporary
-        #     location to do some validation then discovery on internals of the tarfile."""
-
-        #     tar_name = "{}.tar.gz".format(self.get_temp_name())
-        #     tar_path = os.path.realpath(os.path.join(get_downloads_path(), tar_name))
-        #     system.make_directory(os.path.dirname(tar_path))
-
-        #     download_url = self.tarball
-        #     download_untar_fn = functools.partial(
-        #         system.download, download_url, tar_path
-        #     )
-        #     connection_exception_retry(download_untar_fn, 5)
-
-        #     # validate_checksum(self, tar_path)
-        #     # validate_tarfile(self, tar_path)
-        #     self.tarfile_path = tar_path
-
-        if os.path.isfile(self.tarball):
-            # try local first
-            self.tarfile_path = self.tarball
-            tarball_type = 'local'
-        else:
-            # assume download if not local file
-            """copied from /dbt/deps/registry.py install
-            However we can't just package download/untar/install together as one
-            step like in registry. With registry we can simply untar into the
-            packages folder. Here, first we need to download and untar to temporary
-            location to do some validation then discovery on internals of the tarfile."""
-
-            tar_name = "{}.tar.gz".format(self.get_temp_name())
-            tar_path = os.path.realpath(os.path.join(get_downloads_path(), tar_name))
-            system.make_directory(os.path.dirname(tar_path))
-
-            download_url = self.tarball
-            download_untar_fn = functools.partial(
-                system.download, download_url, tar_path
-            )
-            connection_exception_retry(download_untar_fn, 5)
-
-            # validate_checksum(self, tar_path)
-            # validate_tarfile(self, tar_path)
-            self.tarfile_path = tar_path
-            tarball_type = 'remote'
-
-        validate_checksum(self, self.tarfile_path)
-        validate_tarfile(self, self.tarfile_path)
-        fire_event(TarballReceivedFeedback(tarball_type=tarball_type))
-
-        """Assumed structure:
-          Like registry: package at root of tarfile.
-            Example: dbt-utils-0.6.6.tgz -> 
-                /dbt-utils/dbt_project.yml
-            This is default package format this is expected.
-          Like git subdirectory: package is in subdirectory of tarfile.
-            Example: packages-tar.tgz -> /packages-folder/dbt-utils-0.6.6/dbt_project.yml
-            Use subdirectory setting as `packages-folder/dbt-utils-0.6.6` in this case.
-        """
-        with tarfile.open(self.tarfile_path, "r:gz") as tarball:
-            members = tarball.getmembers()
-            tar_size = sum(x.size for x in members)
-            if not tar_size <= TARFILE_MAX_SIZE:
-                msg = "Tarfile size is larger than limit " f"of {TARFILE_MAX_SIZE}."
-                raise DependencyException(msg)
-
-            tarfile_dir_list = [x.name for x in members if x.isdir()]
-            # print(tarfile_dir_list)
-
-            # subdirectory arg not given, get root name
-            if not self.subdirectory:
-                if len(tarfile_dir_list) > 0:
-                    resolved_tar_dir_name = os.path.commonpath(tarfile_dir_list)
-                    # returns "" if multiple candidates for commonpath
-                else:
-                    # e.g. if tarfile has files and no parent dir
-                    resolved_tar_dir_name = ""
-
-                if resolved_tar_dir_name == "":
-                    package_structure_malformed()
-                tar_dir_name = resolved_tar_dir_name
-
-            # user gave subdirectory, check if exists
-            else:
-                # first validate that tarfile has subdirs
-                if len(tarfile_dir_list) < 1:
-                    package_structure_malformed()
-                if self.subdirectory not in tarfile_dir_list:
-                    msg = f"{self.subdirectory} is not a valid dir in tarfile."
-                    raise DependencyException(msg)
-                tar_dir_name = self.subdirectory
-
-        fire_event(
-            UntarProjectRoot(subdirectory=self.subdirectory, tar_dir_name=tar_dir_name)
-        )
-        self.subdirectory = tar_dir_name
-
-        tar_name = self.get_temp_name()
-        untar_path = os.path.realpath(os.path.join(get_downloads_path(), tar_name))
-        system.make_directory(os.path.dirname(untar_path))
-        system.untar_package(self.tarfile_path, untar_path)
-        self.untar_path = untar_path
+        return "version {}".format(self.version)
 
     def _fetch_metadata(self, project, renderer):
-        # have an untared folder in temp storage, will pass
-        # to metadata for subsequent file checks to be performed
-        # on that temp dir
-        tar_dir_name = self.subdirectory
-        tar_extracted_root = os.path.realpath(
-            os.path.join(self.untar_path, tar_dir_name)
-        )
+        """
+        recycle RegistryPackageMetadata so that we can use the install and
+        download_and_untar from RegistryPinnedPackage next.
+        build RegistryPackageMetadata from info passed via packages.yml since no
+        'metadata' service exists in this case.
+        """
+        if self.version:
+            name = "{}.{}".format(self.package, self.version)
+        else:
+            name = self.package
 
-        loaded = Project.from_project_root(tar_extracted_root, renderer)
+        dct = {
+            "name": name,
+            "packages": [],  # note: required by RegistryPackageMetadata
+            "downloads": {"tarball": self.tarball},
+        }
 
-        return ProjectPackageMetadata.from_project(loaded)
+        return RegistryPackageMetadata.from_dict(dct)
 
     def install(self, project, renderer):
-        """copied from /dbt/deps/registry.py install
-        We can't just package download/untar/install together as one
-        step like in registry. So far we've untarred to a temp location
-        scanned the tarfile for folder structure, and determined where
-        in the tarfile the package is located, and destination folder
-        name in the packages install location. Do a lot of recycling here
-        mostly just building out paths for final copy operation. Temp untar
-        location + subdir if needed -> package install + dest folder name"""
-        tar_name = self.get_temp_name()
-        untar_path = os.path.realpath(os.path.join(get_downloads_path(), tar_name))
-        system.make_directory(os.path.dirname(untar_path))
-        system.untar_package(self.tarfile_path, untar_path)
+        """
+        verbatim from dbt.deps.registry.RegistryPinnedPackage
+            install() from RegistryPinnedPackage class
+            dbt crew to refactor this to more general place so it can be reused here?
+        this should be moved to dbt.deps.base, or to a dbt.deps.common file, waiting on 
+        dbt labs feedback on how to proceed (or leave as is)
+        """
+        metadata = self.fetch_metadata(project, renderer)
 
-        tar_dir_name = self.subdirectory
-        source_path = os.path.realpath(os.path.join(untar_path, tar_dir_name))
+        tar_name = "{}.{}.tar.gz".format(self.package, self.version)
+        tar_path = os.path.realpath(os.path.join(get_downloads_path(), tar_name))
+        system.make_directory(os.path.dirname(tar_path))
 
+        download_url = metadata.downloads.tarball
         deps_path = project.packages_install_path
-        fire_event(CopyFeedback(source_path=source_path, dest_path=deps_path))
+        package_name = self.get_project_name(project, renderer)
 
-        system.move(source_path, deps_path)
+        download_untar_fn = functools.partial(
+            self.download_and_untar, download_url, tar_path, deps_path, package_name
+        )
+        connection_exception_retry(download_untar_fn, 5)
+
+    def download_and_untar(self, download_url, tar_path, deps_path, package_name):
+        """
+        verbatim from dbt.deps.registry.RegistryPinnedPackage
+            download_and_untar() from RegistryPinnedPackage class
+            dbt crew to refactor this to more general place so it can be reused here?
+        this should be moved to dbt.deps.base, or to a dbt.deps.common file, waiting on 
+        dbt labs feedback on how to proceed (or leave as is)
+        """
+
+        """
+        Sometimes the download of the files fails and we want to retry.  Sometimes the
+        download appears successful but the file did not make it through as expected
+        (generally due to a github incident).  Either way we want to retry downloading
+        and untarring to see if we can get a success.  Call this within
+        `_connection_exception_retry`
+        """
+
+        system.download(download_url, tar_path)
+        system.untar_package(tar_path, deps_path, package_name)
 
 
-class TarballUnpinnedPackage(
-    TarballPackageMixin, UnpinnedPackage[TarballPinnedPackage]
-):
+class TarballUnpinnedPackage(TarballPackageMixin, UnpinnedPackage[TarballPinnedPackage]):
     def __init__(
         self,
         tarball: str,
-        sha1: Optional[str] = None,
-        subdirectory: Optional[str] = None,
+        package: str,
+        version: Optional[str] = None,
     ) -> None:
         super().__init__(tarball)
-        self.sha1 = sha1
-        self.subdirectory = subdirectory
+        # setup to recycle RegistryPinnedPackage fns
+        self.package = package
+        self.version = version
 
     @classmethod
     def from_contract(cls, contract: TarballPackage) -> "TarballUnpinnedPackage":
         return cls(
             tarball=contract.tarball,
-            sha1=contract.sha1,
-            subdirectory=contract.subdirectory,
+            package=contract.name,
+            version=contract.version,
         )
 
     def incorporate(self, other: "TarballUnpinnedPackage") -> "TarballUnpinnedPackage":
         return TarballUnpinnedPackage(
-            tarball=self.tarball, sha1=self.sha1, subdirectory=self.subdirectory
+            tarball=self.tarball, package=self.package, version=self.version
         )
 
     def resolved(self) -> TarballPinnedPackage:
         return TarballPinnedPackage(
-            tarball=self.tarball, sha1=self.sha1, subdirectory=self.subdirectory
+            tarball=self.tarball, package=self.package, version=self.version
         )
