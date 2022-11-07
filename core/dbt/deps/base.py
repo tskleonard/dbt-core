20a77a6497942096577beea046895a59742ab351
timle <aplenty_blocs_0z@icloud.com>
2022-11-07 10:42:31 -0500

    refactor install and download to upstream PinnedPackage class
    i'm on the fence if this is right approach, but seems like most sensible after some thought

--- core/dbt/deps/base.py
+++ core/dbt/deps/base.py
@@ -1,5 +1,6 @@
 import abc
 import os
+import functools
 import tempfile
 from contextlib import contextmanager
 from typing import List, Optional, Generic, TypeVar
@@ -8,6 +9,7 @@ from dbt.clients import system
 from dbt.contracts.project import ProjectPackageMetadata
 from dbt.events.functions import fire_event
 from dbt.events.types import DepsSetDownloadDirectory
+from dbt.utils import _connection_exception_retry as connection_exception_retry
 
 DOWNLOADS_PATH = None
 
@@ -96,6 +98,48 @@ class PinnedPackage(BasePackage):
 
     def get_subdirectory(self):
         return None
+
+    def _install(self, project, renderer):
+        """
+        verbatim from dbt.deps.registry.RegistryPinnedPackage
+            install() from RegistryPinnedPackage class
+            Q for dbt labs, is this an appropriate pattern to refactor against?
+            used upstream in RegistryPinnedPackage and TarballPinnedPackage via inheritance
+            can't overwrite install() since it would be inherited by GitPinnedPackage as well
+        """
+        metadata = self.fetch_metadata(project, renderer)
+
+        tar_name = "{}.{}.tar.gz".format(self.package, self.version)
+        tar_path = os.path.realpath(os.path.join(get_downloads_path(), tar_name))
+        system.make_directory(os.path.dirname(tar_path))
+
+        download_url = metadata.downloads.tarball
+        deps_path = project.packages_install_path
+        package_name = self.get_project_name(project, renderer)
+
+        download_untar_fn = functools.partial(
+            self.download_and_untar, download_url, tar_path, deps_path, package_name
+        )
+        connection_exception_retry(download_untar_fn, 5)
+
+    def download_and_untar(self, download_url, tar_path, deps_path, package_name):
+        """
+        verbatim from dbt.deps.registry.RegistryPinnedPackage
+            download_and_untar() from RegistryPinnedPackage class
+            Q for dbt labs, is this an appropriate pattern to refactor against?
+            used upstream in RegistryPinnedPackage and TarballPinnedPackage via inheritance
+        """
+
+        """
+        Sometimes the download of the files fails and we want to retry.  Sometimes the
+        download appears successful but the file did not make it through as expected
+        (generally due to a github incident).  Either way we want to retry downloading
+        and untarring to see if we can get a success.  Call this within
+        `_connection_exception_retry`
+        """
+
+        system.download(download_url, tar_path)
+        system.untar_package(tar_path, deps_path, package_name)
 
 
 SomePinned = TypeVar("SomePinned", bound=PinnedPackage)
